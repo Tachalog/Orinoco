b9db9a941258a2e02ce418c8c8bb192aa49a7adb
NLO <logerot.natacha@gmail.com>
2020-08-21 07:50:44 +0200

    Modification de fichier CSS et json

--- JS/selection.js
+++ JS/selection.js
@@ -1,2 +1,13 @@
+let selectCam = document.querySelector('input');
 
+selectCam.addEventListener('click', function (e) {
+    e.preventDefault();
+});
 
+selectCam.onchange = function () {
+    if (selectCam.checked) {
+        selectCam.textContent = "Appareil sélectionné";
+    } else {
+        selectCam.textContent = "Merci de sélectionner un produit.";
+    }
+};
