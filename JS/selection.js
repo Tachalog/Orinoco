ad34e0d1a403640631b9e0bfa0cff444b55e2945
NLO <logerot.natacha@gmail.com>
2020-09-02 10:36:13 +0200

    Modifications sur JavaScript et HTML

---
+++ JS/selection.js
@@ -0,0 +1,27 @@
+/* Ce petit bout de code fonctionne
+var btn = document.querySelectorAll('button');
+btn = confirm("continuez vos achats ?");
+*/
+
+
+var btnPanier = document.querySelectorAll('button');
+
+for (var i = 0; i < btnPanier.length; i++){
+
+    var button = btnPanier[i];
+    button.addEventListener('click', function (e){
+
+        window.alert('clique sur le bouton')
+    })
+}
+
+
+
+
+
+
+var values = document.querySelectorAll('h3')
+console.log(values);
+
+
+
